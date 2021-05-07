827819243bdb9fefc282696114e86765cda8d088
JSKitty <loopylew113@protonmail.com>
2021-05-08 00:24:58 +0100

    [SCP] [UI] Finish SCP-2 & Add Staking GUI
    The wallet now has a minimalistic Staking GUI which allows one-click claiming / redeeming of stake rewards, the full staking logic has been implemented, as well as send/receive logic.

--- public/js/switchPages.js
+++ public/js/switchPages.js
@@ -5,6 +5,7 @@ function switchToLogin() {
   domReceivePage.style.display = "none";
   domSendPage.style.display = "none";
   domAuthPage.style.display = "none";
+  domStakingPage.style.display = "none";
 
   // Show
   domLoginPage.style.display = "block";
@@ -16,6 +17,7 @@ function switchToSend() {
   domReceivePage.style.display = "none";
   domLoginPage.style.display = "none";
   domAuthPage.style.display = "none";
+  domStakingPage.style.display = "none";
 
   // Show
   domSendPage.style.display = "block";
@@ -39,6 +41,7 @@ function switchToDashboard() {
   domReceivePage.style.display = "none";
   domLoginPage.style.display = "none";
   domAuthPage.style.display = "none";
+  domStakingPage.style.display = "none";
 
   // Show
   domDashboardPage.style.display = "block";
@@ -60,6 +63,7 @@ function switchToReceive() {
   domDashboardPage.style.display = "none";
   domLoginPage.style.display = "none";
   domAuthPage.style.display = "none";
+  domStakingPage.style.display = "none";
   getBalance(true);
 
   // Show
@@ -81,6 +85,7 @@ function switchToAuth() {
   domDashboardPage.style.display = "none";
   domLoginPage.style.display = "none";
   domReceivePage.style.display = "none";
+  domStakingPage.style.display = "none";
 
   // Show
   domAuthPage.style.display = "block";
@@ -93,4 +98,33 @@ function switchToAuth() {
 
   // Add active
   dom2FABtn.classList.add('active');
+}
+
+function switchToStaking(contract) {
+  // Hide
+  domSendPage.style.display = "none";
+  domDashboardPage.style.display = "none";
+  domLoginPage.style.display = "none";
+  domReceivePage.style.display = "none";
+
+  // Show
+  domStakingPage.style.display = "block";
+  domHeader.style.display = "block";
+
+  let cToken = TOKENS.getToken(contract);
+  let cAccount = cToken.getAccount(pubkeyMain);
+  let cStatus = cToken.getStakingStatus(pubkeyMain);
+  // put token + account info on the Staking Page on this refresh!
+  domStakingTitle.innerText = cToken.name + " " + cToken.ticker;
+  if (cStatus.enabled)
+    domStakingSubtitle.innerHTML = "You've been staking for " + cStatus.age.toLocaleString('en-GB') + " blocks and earned...";
+  else
+    domStakingSubtitle.innerHTML = "You've transacted tokens recently, so you must wait another " + (cToken.minAge - cStatus.age).toLocaleString('en-GB') + " blocks before you can start receiving stakes again!";
+  
+  domStakingRewards.innerHTML = (cAccount.unclaimed_balance / COIN).toLocaleString('en-GB') + " " + cToken.ticker;
+  domStakingRedeem.setAttribute('onclick', 'claimStakingRewards("' + cToken.contract + '")');
+  if (cAccount.unclaimed_balance > 0) {
+    domStakingRedeem.classList.remove('disabled');
+    domStakingRedeem.innerText = 'Claim Rewards';
+  }
 }
