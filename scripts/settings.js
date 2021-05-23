49069ec3a6bbd400396fdbeb6b466eeca905e868
JSKitty <loopylew113@protonmail.com>
2021-05-24 00:11:34 +0100

    [UI] Add theme switcher
    A placeholder sun/moon button in the bottom-left of the wallet which instantly switches the current theme between Light/Dark mode.
    
    Dark mode CSS sheets are not done yet, this is just the switcher functionality, the actual theme CSS will be done shortly!

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,6 @@
-//Settings Defaults
+// Settings Defaults
 var debug = false;
+
 // TODO: The below variables need some serious cleanup!
 //------------------------------
 var trx;
@@ -7,3 +8,25 @@ var amountOfTransactions;
 var balance;
 var fee;
 var walletAlreadyMade = 0;
+
+/* Theme and UI settings */
+var domTheme;
+var domThemeButton;
+var cssThemes = ["light", "dark"];
+
+// Load the next CSS theme sheet from disk
+function loadNextTheme() {
+  // Update theme
+  let curIndex = Number(domTheme.getAttribute("themeIndex"));
+  let nextIndex = cssThemes[curIndex + 1] ? curIndex + 1 : 0;
+  domTheme.href = "style/latest-style-" + cssThemes[nextIndex] + ".css";
+  domTheme.setAttribute("themeIndex", nextIndex);
+  // Update theme button
+  if (domThemeButton.classList.contains("fa-sun")) {
+    domThemeButton.classList.remove("fa-sun");
+    domThemeButton.classList.add("fa-moon");
+  } else {
+    domThemeButton.classList.remove("fa-moon");
+    domThemeButton.classList.add("fa-sun");
+  }
+}
