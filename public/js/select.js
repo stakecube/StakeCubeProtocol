1991d3a52a781d1aaefd3f4b00289e1775ef4332
JSKitty <loopylew113@protonmail.com>
2021-05-26 22:57:48 +0100

    [Stability] Further enable Strict Mode
    Enable Strict Mode on more parts of the codebase, this also revealed some small bugs in the UI Index which were promptly fixed in the same patch, stability and security FTW!

--- public/js/select.js
+++ public/js/select.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var coinArray = [];
 function refreshCoinSelector() {
   coinArray = [];
--- public/js/switchPages.js
+++ public/js/switchPages.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function switchToLogin() {
   // Hide
   domHeader.style.display = "none";
